decc92a43fd86aba9fe8f9875767dc4485f97aaf
nickdee1 <nick.dee1@post.cz>
2019-04-27 23:33:34 +0200

    Simple UDP Server/Client app functionality (send/receive file)

--- venv/lib/python3.7/client.py
+++ venv/lib/python3.7/client.py
@@ -1,1 +1,42 @@
-# TODO: - Implement receive and process methods
+import socket
+
+
+class Client:
+
+    def __init__(self, server_ip, server_port):
+        self.server_ip = server_ip
+        self.server_port = server_port
+        self.buffer_size = 1024
+        self.UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        self.file_name = None
+        self.file_size = None
+
+    def send_file_download_request(self, filename):
+        print("Sending request to the server...")
+        self.file_name = filename
+        filename_data = str(filename).encode()
+
+        self.UDPClientSocket.sendto(filename_data, (self.server_ip, self.server_port))
+
+        self.file_size = self.UDPClientSocket.recvfrom(1024)
+        self.file_size = int(self.file_size[0].decode())
+
+    def receive_file(self, with_name):
+        print("Receiving file...")
+        received_file_size = 0
+        output_file = open(with_name, "ab")
+
+        while True:
+            received_bytes = self.UDPClientSocket.recvfrom(self.buffer_size)
+            output_file.write(received_bytes[0])
+            received_file_size += 1024
+
+            if received_file_size > self.file_size:
+                print("File was received and saved as ", with_name)
+                break
+
+
+if __name__ == "__main__":
+    client = Client("127.0.0.1", 20001)
+    client.send_file_download_request("10mb.jpg")
+    client.receive_file(with_name="new_file.jpg")
--- venv/lib/python3.7/server.py
+++ venv/lib/python3.7/server.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 
 class Server:
@@ -9,10 +10,47 @@ class Server:
         self.buffer_size = 1024
         self.UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
         self.UDPServerSocket.bind((server_ip, server_port))
+        self.client_address = None
+        self.file_name = None
 
-    # TODO: - implement reading and sending file methods
-    def process_file(self, file_name):
-        byte_array = None
-        with open(file_name, "rb") as input_file:
-            file_bytes = input_file.read()
-            byte_array = bytearray(file_bytes)
+    def process_file(self):
+        print("Sending...")
+        file_to_send = open(self.file_name, "rb")
+        file_bytes = bytearray(file_to_send.read())
+
+        self.UDPServerSocket.sendto(str(len(file_bytes)).encode(), self.client_address)
+
+        start_index = 0
+        end_index = self.buffer_size
+
+        while True:
+            buffered_bytes = file_bytes[start_index:end_index]
+
+            self.UDPServerSocket.sendto(buffered_bytes, self.client_address)
+            time.sleep(0.0001)
+            start_index += self.buffer_size
+            end_index += self.buffer_size
+
+            if start_index > len(file_bytes):
+                print("File was sent!")
+                break
+
+    def receive_file_name(self):
+        received_bytes = self.UDPServerSocket.recvfrom(self.buffer_size)
+        self.client_address = received_bytes[1]
+        self.file_name = received_bytes[0].decode()
+
+        print(self.file_name, " was requested to be sent")
+        try:
+            open(self.file_name, "rb")
+        except FileNotFoundError:
+            print("File with name ", self.file_name, " does not exist")
+            self.file_name = None
+            return
+        return
+
+
+if __name__ == "__main__":
+    server = Server("127.0.0.1", 20001)
+    server.receive_file_name()
+    server.process_file()
